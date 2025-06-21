05651c59cf50437a31d81357e9f765cd699ad68d
Satya-S3 <satyasahu644@gmail.com>
2025-06-21 12:19:45 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# React Hook Form (with Zod) - Full Tutorial 2024 🔥🔥
+## https://youtu.be/qyzznUNe1ho
+
+![react hook form](https://github.com/user-attachments/assets/d13d5922-f6cf-4a2b-8b0e-6dc287a194ef)
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import ReactHookFormWithZod from "./components/3-react-hook-form-with-zod";
+
+function App() {
+  return (
+    <div>
+      <h1>React Hook Form Tutorial</h1>
+      <ReactHookFormWithZod />
+    </div>
+  );
+}
+
+export default App;
